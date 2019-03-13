ac0b5e691e0db12e5a53b92f2778e1a2d972679f
Yan zhiwen <zhiwen.18@intl.zju.edu.cn>
2019-03-13 18:45:42 +0800

    add

---
+++ Practical4/collatz.py
@@ -0,0 +1,8 @@
+n=88
+print(n)
+while n>1:
+    if n%2 ==0:
+        n=n/2
+    else:
+        n=n*3+1
+    print(n)
---
+++ Practical4/variables.py
@@ -0,0 +1,17 @@
+a=223
+b=223223
+if b%7 == 0:
+    print(b)
+c=b/7
+d=c/11
+e=d/13
+if a>e:
+    print(a)
+else:
+    print(e)
+    
+X=1>0
+Y=1<0
+Z=(X and not Y)or(Y and not X)
+W= X!=Y
+print(X,Y,Z,W)
