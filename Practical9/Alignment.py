37549bc82e255e72436fda1e17ce4679aa681ed3
Yan zhiwen <zhiwen.18@intl.zju.edu.cn>
2019-04-17 11:38:20 +0800

    4.17

---
+++ Practical9/Alignment.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 16 19:36:35 2019
+
+@author: yanzhiwen
+"""
+# input BLOSUM62 matrix in an excel form
+import xlrd
+blosum62 = xlrd.open_workbook(r'1.xlsx')
+sheet1 = blosum62.sheet_by_name('Sheet1')
+
+#input three sequences and match each animo aicds to the table
+humanseq='MLSRAVCGTSRQLAPVLAYLGSRQKHSLPDLPYDYGALEPHINAQIMQLHHSKHHAAYVNNLNVTEEKYQEALAKGDVTAQIALQPALKFNGGGHINHSIFWTNLSPNGGGEPKGELLEAIKRDFGSFDKFKEKLTAASVGVQGSGWGWLGFNKERGHLQIAACPNQDPLQGTTGLIPLLGIDVWEHAYYLQYKNVRPDYLKAIWNVINWENVTERYMACKK'
+mouseseq='MLCRAACSTGRRLGPVAGAAGSRHKHSLPDLPYDYGALEPHINAQIMQLHHSKHHAAYVNNLNATEEKYHEALAKGDVTTQVALQPALKFNGGGHINHTIFWTNLSPKGGGEPKGELLEAIKRDFGSFEKFKEKLTAVSVGVQGSGWGWLGFNKEQGRLQIAACSNQDPLQGTTGLIPLLGIDVWEHAYYLQYKNVRPDYLKAIWNVINWENVTERYTACKK'
+randomseq='WNGFSEWWTHEVDYNQKLTIENNQRPKIHEHEQWGLRQSPPPPKLCCPTCQMCERMRHQNRFAPLMEVGCRCMCWFHDWWVISVGTWLHTVIMYMMWPKRFHHNECPKACFRTTYTRKNHHALYWMLFEMCCYDQDVVWSKTHIFTTVRDIEVYVEQVFFIWGPLCHVAIACYEPVKTIRRRIPMYLCRHCIRGDNSYLLACCSIIYYFYHHMSYYGVLDIL'
+
+dict = {'A':0,'R':1,'N':2,'D':3,'C':4,'Q':5,'E':6,'G':7,'H':8,'I':9,'L':10,'K':11,'M':12,'F':13,'P':14,'S':15,'T':16,'W':17,'Y':18,'V':19}
+
+#define a function that could compare two sequence
+def alignment(aseq,bseq):
+    newalignment=[]
+    edit_distance = 0
+    score=0
+    #match each animo acids in two seq with the matrix and find out the score
+    for i in range(len(aseq)): 
+        a = aseq[i]
+        b = bseq[i]
+        line = dict[a]  
+        row = dict[b]
+        score += sheet1.cell(line,row).value
+        #this is for bonus project if score>=0 then add + 
+        #and wirtten in blast-like way
+        #also count the edit_distance
+        if aseq[i]!=bseq[i] and score >= 0:
+            edit_distance += 1 
+            newalignment.append('+')
+        if aseq[i]!=bseq[i] and score < 0:
+            edit_distance += 1 
+            newalignment.append('_')
+        if aseq[i] == bseq[i]:
+            newalignment.append(humanseq[i])
+
+#print out all the result
+    print(edit_distance)
+    print(score)
+    #print(finalscore/len(aseq))
+    print(aseq)
+    print("".join(newalignment))
+    print(bseq)
+
+alignment(humanseq,mouseseq)
---
+++ feedback_Yan_Zhiwen.md
@@ -0,0 +1,30 @@
+# This is your formative feedback for the IBI1 portfolio.
+
+
+## Please do the following:
+
+- [ ] Add this file to your GitHub fork of IBI1_2018-19. You are allowed to delete the last line (giving your score) before uploading the file to GitHub, if you do not want others to see it. 
+- [ ] Make changes to your code from weeks 3 to 5 according to the feedback below. This will form part of your summative assessment. (You may use the checkboxes to keep track of your progress) 
+
+
+## Feedback:
+
+### Feedback Week 3:
+
+- [ ]  The folder for Practical 3 should contain a file called thingsIlike.txt that lists things you like. Please push this to your GitHub repo.
+- [ ] The folder for Practical 3 should contain a file called Reflection.txt that contains a few reflections on the practical.
+
+### Feedback Week 4:
+
+- [ ] In file collatz.py there is no pseudocode that documents the code. Add a little bit of pseudocode to explain what parts of the script (and the script as a whole) do. 
+
+### Feedback Week 5:
+
+The code for week 5 is great. There is no need to change anything.
+
+
+
+
+This asssesment is formative and does not form part of your final grade. 
+
+If this was a summative assessment, you would have scored 87 out of 100
